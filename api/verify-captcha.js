165ea7176028568791b62d0249948742993eebb3
Kirill Batomunkuev <i@batomunkuevv.ru>
2024-12-14 11:03:38 +0900

    change

--- api/verify-captcha.js
+++ api/verify-captcha.js
@@ -1,6 +1,6 @@
-const fetch = require("node-fetch");
+export default async (req, res) => {
+    const { default: fetch } = await import('node-fetch');
 
-module.exports = async (req, res) => {
     res.setHeader("Access-Control-Allow-Origin", "*");
     res.setHeader("Access-Control-Allow-Methods", "GET,POST,OPTIONS");
     res.setHeader("Access-Control-Allow-Headers", "Content-Type");
