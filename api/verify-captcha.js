00e8bd7a5c765e235146be2d688a7d9b2d0e93c2
Kirill Batomunkuev <i@batomunkuevv.ru>
2024-12-14 10:59:33 +0900

    add headers

--- api/verify-captcha.js
+++ api/verify-captcha.js
@@ -1,6 +1,10 @@
 import fetch from "node-fetch";
 
 module.exports = async (req, res) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Methods", "GET,POST,OPTIONS");
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+
     if (req.method !== "POST") {
         return res.status(405).send({ message: "Method Not Allowed" });
     }
